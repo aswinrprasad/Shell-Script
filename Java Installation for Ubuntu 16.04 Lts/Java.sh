5d99958659182e96ccf004913b6d69496b5803d3
Aswin R Prasad <aswinrprasad@gmail.com>
2017-09-20 17:27:19 +0530

    Java Installation Scrpt

---
+++ Java Installation for Ubuntu 16.04 Lts/Java.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "This shell script is created by Aswin R Prasad to download and install Java.."
+echo "Running all the commands..This might take a few minutes.. "
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install default-jre -y
+sudo apt-get install default-jdk -y
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install java-common oracle-java8-installer
+sudo update-alternatives --config java
+cd /etc
+echo "export JAVA_HOME="/usr/lib/jvm/java-1.8.0-openjdk-1.8.1.51-1.b16.el7_1.x86_64"" >> profile
+source /etc/profile
+echo "The Installation is Complete ..."
+echo "Checking if java is installed and it's version ..."
+java -version
